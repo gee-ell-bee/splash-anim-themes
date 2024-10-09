13120ceb9da293eb919d949029e9f3d53a64d039
geebee <grey@glbweb.design>
2024-10-09 16:51:46 -0700

    + css h1 & btn animations, + replay & cont btns, + cont btn js

---
+++ main.js
@@ -0,0 +1,37 @@
+// global vars
+const openBtn = document.getElementById("openBtn");
+const replayBtn = document.getElementById("replayBtn");
+
+console.log(openBtn, replayBtn);
+
+// functions
+
+window.addEventListener("DOMContentLoaded", domStart());
+
+function domStart() {
+    openBtn.addEventListener("click", clickOpen);
+    addClass(btnList(), "visible");
+};
+
+function btnList() {
+    return newBtnList = [openBtn, replayBtn];
+};
+
+function addClass(elems, className) {
+    elems.forEach(elem => {
+        elem.classList.add(className);
+    });
+};
+
+function clickOpen() {
+    const content = document.getElementById("content");
+    // console.log(content);
+    expandMain();
+    openBtn.onclick(content.classList.add("visible"));
+};
+
+function expandMain() {
+    const main = document.querySelector("main");
+    console.log(main);
+    main.classList.add("lengthen");
+};
