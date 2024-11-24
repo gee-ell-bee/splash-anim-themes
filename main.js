5d83d21020a3efef997dd6b5612db18af75d6d05
geebee <grey@glbweb.design>
2024-11-23 18:03:11 -0800

    + jqueryColor, successfully used 1 time

--- main.js
+++ main.js
@@ -13,5 +13,17 @@ $(document).ready(function() {
         marginTop: "0",
         marginLeft: "-20vw"}, 3800, "swing"
     );
+
+    $("#blueB").on("click", function() {
+        console.log("clicked!");
+        $(".bg.red").animate({
+            backgroundColor: jQuery.Color("rgb(193, 43, 43)")
+        }, "slow");
+    });
     
 });
+
+function animButtons(elem, {css}) {
+    elem.animate({css}, 1000, "swing");
+    return elem;
+}
